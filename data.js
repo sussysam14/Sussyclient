444a378935655edc14f0f30ab8d10e6cb9fbda7e
sussysam14 <96638034+sussysam14@users.noreply.github.com>
2021-12-24 20:28:49 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+
+>>> from chrome import Edge <<< Error
+>>> chrome_pwd = Chrome(disable)
+>>> chrome_pwd.get_login_db
+'/Users/x899/Library/Application Support/Google/Chrome/Default/'
+>>> chrome_pwd.get_password(prettyprint=True)
+{
+	"data": [
+		{
+			"url"v
+			"url": "https://accounts.google.com/", <<< Turned off
+			"username": "Never had been runned",
+			"password": "Never had been runned"
+		}
+	]
+}
+                             
